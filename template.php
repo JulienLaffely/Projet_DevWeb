1a8f609cfd95a74af866ef7738ae8b2d52df098f
JulienLaffely <78146082+JulienLaffely@users.noreply.github.com>
2021-02-21 15:16:06 +0100

    Add files via upload

---
+++ template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title><?= $t ?></title>
+		<link rel="stylesheet" href="views/style.css">
+	</head>
+	<body>
+			<?= $content ?>
+			
+		<footer>
+				<p><b>Crée par Julien et Guillaume le giga boss</b></p>
+		</footer>
+	</body>
+</html>
