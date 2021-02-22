28b098f02acf36c4508a0ea4fa46048e71c49613
JulienLaffely <78146082+JulienLaffely@users.noreply.github.com>
2021-02-22 16:28:47 +0100

    Add files via upload

--- views/template.php
+++ views/template.php
@@ -5,9 +5,22 @@
 		<title><?= $t ?></title>
 		<link rel="stylesheet" href="views/style.css">
 	</head>
-	<body>
+	<body class="area">
+			<ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+          	</ul>
+            <div id="corps">
 			<?= $content ?>
-			
+			</div>
 		<footer>
 				<p><b>Crée par Julien et Guillaume le giga boss</b></p>
 		</footer>
