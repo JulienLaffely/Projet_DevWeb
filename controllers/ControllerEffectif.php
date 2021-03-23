35c5b7975cb97e7f95816ce2259aa84b753f863e
JulienLaffely <78146082+JulienLaffely@users.noreply.github.com>
2021-03-23 10:06:05 +0100

    Add files via upload

--- controllers/ControllerEffectif.php
+++ controllers/ControllerEffectif.php
@@ -23,15 +23,17 @@
 					<tr>
 						<td id='tde'><b>NOM</b></td>
 						<td id='tde'><b>PRENOM</b></td>
+						<td id='tde'><b>DATE DE NAISSANCE</b></td>
 				 	</tr>";
 			foreach ($this->_model->getAllJoueurs() as $joueur) {
 				echo "	<tr>
 							<td id='tde'>$joueur[0]</td>
 							<td id='tde'>$joueur[1]</td>
+							<td id='tde'>$joueur[2]</td>
 						</tr>";
 
 			}
-			echo "</table>";
+			echo "</table></br>";
 		}
 
 		public function AfficheEnregistrementJoueur()
@@ -41,9 +43,12 @@
 					<legend>Enregistrement de nouveau joueur</legend>
 					<label>Nom</label>
 					<input type='text' name='NomNewJoueur'/>
-					<label style='margin-left: 100px'>Prenom</label>
+					<label style='margin-left: 150px'>Prenom</label>
 					<input type='text' name='PrenomNewJoueur'/>
-					<input type='submit' name='NewJoueur' id='BoutAjoutJoueur' value='Enregistrer un nouveau joueur' style='margin-left: 100px;height:50px;background-color:green'/>
+					<label style='margin-left: 140px'>Date de naissance</label>
+					<input type='date' min='1950-01-01' max='2002-01-01' name='DdnNewJoueur'/>
+					</br></br>
+					<input type='submit' name='NewJoueur' id='BoutAjoutJoueur' value='Enregistrer un nouveau joueur' style='margin-left: 370px;height:50px;background-color:green'/>
 					</fieldset>
 				</form>";
 		}
--- models/modelEffectif.php
+++ models/modelEffectif.php
@@ -7,17 +7,36 @@ class modelEffectif extends Model
 	private $_joueurs;
 	public function __construct()
 	{
-		$bdd=$this->getBdd();
-		$this->_joueurs=$bdd->query('SELECT nom , prenom FROM joueurs');
+		
 	}
 
 	public function getAllJoueurs(){
-		return $this->_joueurs;
+		$bdd=$this->getBdd();
+		return $bdd->query("SELECT nom , prenom , DATE_FORMAT(ddn,'%d-%m-%Y') FROM joueurs");
 	}
 
-	public function ActualisationBDD()
+	public function AjoutJoueurBDD()
 	{
-		
+		$nom=$_POST['NomNewJoueur'];
+		$prenom=$_POST['PrenomNewJoueur'];
+		$ddn=$_POST['DdnNewJoueur'];
+		if($nom==""||$prenom==""||$ddn==""){
+			echo "<script>alert('Le nouveau joueur doit avoir un nom , un prenom et une date de naissance !')</script>";
+		}
+		else {
+			$bdd=$this->getBdd();
+			$query= $bdd->query("SELECT count(*) AS total FROM joueurs WHERE nom='$nom' AND prenom='$prenom' AND ddn='$ddn'");
+			$query = $query->fetchAll(PDO::FETCH_ASSOC);
+			if($query[0]['total']==0){
+				$query = $bdd->query("SELECT count(*) AS IDS FROM joueurs");
+				$query = $query->fetchAll(PDO::FETCH_ASSOC);
+				$id=((int)($query[0]['IDS']))+1;
+				$bdd->query("INSERT INTO joueurs VALUES ($id,'$nom','$prenom','$ddn')");
+			}
+			else{
+				echo "<script>alert('Attention ! Joueur déja enregistrer dans le club !')</script>";
+			}
+		}
 	}
 
 }
