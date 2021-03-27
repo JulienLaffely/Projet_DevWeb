bccba85b4f3a7222b01daad187714dc001911e94
JulienLaffely <78146082+JulienLaffely@users.noreply.github.com>
2021-03-27 18:30:49 +0100

    Delete views directory

--- views/viewConnexion.php
+++
@@ -1,9 +0,0 @@
-<?php $this->_t = 'Centre de controle'; ?>
-<center>
-<form id="bouttons4" method="post">
-<input id="test" type="submit" name="onglet" value="Calendrier" />
-<input type="submit" name="onglet" value="Planning Absences" />
-<input type="submit" name="onglet" value="Convocation"/>
-<input type="submit" name="onglet" value="Effectif"/>
-<input type="submit" name="onglet" value="Accueil" />
-</center>
--- views/viewEffectif.php
+++
@@ -1,9 +0,0 @@
-<?php $this->_t = 'Effectif'; ?>
-<center>
-<form id="bouttons4" method="post">
-<input id="test" type="submit" name="onglet" value="Calendrier" />
-<input type="submit" name="onglet" value="Planning Absences" />
-<input type="submit" name="onglet" value="Convocation" />
-<input type="submit" name="onglet" value="Effectif"  disabled/>
-<input type="submit" name="onglet" value="Accueil" />
-</center>
